2d632b3ea2db25904e59ca77d568d878143d4010
Forest Park <foresthpark@gmail.com>
2022-11-06 12:21:59 -0700

    add clear command to help

--- src/constants/outputs/other.ts
+++ src/constants/outputs/other.ts
@@ -1,7 +1,8 @@
 import * as outputs from "@/constants/outputs";
 
 export const help = async (): Promise<string> => {
-  const commands = Object.keys(outputs).sort().join(", ");
+  const commands = Object.keys(outputs);
+  const allCommands = ["clear", ...commands].sort().join(", ");
 
-  return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.`;
+  return `Available commands:\n${allCommands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.`;
 };
