7aca48894d93a18ff2543522fbd8e7f2cb30b053
Forest Park <foresthpark@gmail.com>
2025-04-02 11:19:53 -0700

    Add Sharewear and Software Developers Calgary projects

--- src/constants/projectlist.ts
+++ src/constants/projectlist.ts
@@ -7,28 +7,28 @@ export interface Project {
 
 export const projectlist: Project[] = [
   {
+    name: "Sharewear",
+    website: "https://sharewearprofile.com",
+    github: null,
+    tech: ["NextJS", "Tailwind CSS", "NeonDB", "tRPC"],
+  },
+  {
+    name: "Software Developers Calgary",
+    website: "https://sdc.fyi/",
+    github: null,
+    tech: ["NextJS", "Tailwind CSS", "NeonDB", "tRPC"],
+  },
+  {
     name: "Terminal Portfolio",
     website: "https://terminal.forestp.dev",
     github: "https://github.com/foresthpark/terminal-portfolio",
     tech: ["Next.js", "Tailwind CSS"],
   },
   {
-    name: "URL Shortener",
-    website: "https://u.forestp.dev",
-    github: "https://github.com/foresthpark/shortlink_trpc",
-    tech: ["NextJS", "PlanetScale DB", "tRPC"],
-  },
-  {
     name: "Music Player Prototype",
     website: "https://arcadia.forestp.dev",
     github: null,
     tech: ["NextJS", "Redux Toolkit", "Tailwind CSS"],
-  },
-  {
-    name: "Ketchup Wars",
-    website: "https://ketchup.forestp.dev",
-    github: "https://github.com/foresthpark/ketchup_wars",
-    tech: ["NextJS", "PlanetScale DB"],
   },
   {
     name: "Keto Clock",
@@ -42,12 +42,12 @@ export const projectlist: Project[] = [
       "Redux Toolkit",
     ],
   },
-  {
-    name: "EMCO Energy",
-    website: "https://emcoenergy.com",
-    github: null,
-    tech: ["NextJS", "MongoDB", "Firebase", "Lambda", "EC2", "DynamoDB"],
-  },
+  // {
+  //   name: "EMCO Energy",
+  //   website: "https://emcoenergy.com",
+  //   github: null,
+  //   tech: ["NextJS", "MongoDB", "Firebase", "Lambda", "EC2", "DynamoDB"],
+  // },
   {
     name: "Greengate Energy VR Experience",
     website: "https://youtu.be/3OQSe2HRfJI",
@@ -67,34 +67,14 @@ export const projectlist: Project[] = [
     tech: ["NextJS", "Firebase", "Clarifai"],
   },
   {
-    name: "Inuka",
-    website: "https://inuka.forestparkdev.ca",
-    github: null,
-    tech: ["Flutter", "Firebase"],
-  },
-  {
-    name: "Expense Robot",
-    website: "https://expenserobot.forestparkdev.ca/#/",
-    github: null,
-    tech: ["Flutter", "Firebase"],
-  },
-  {
     name: "PaintJS",
     website: "https://foresthpark.github.io/PaintJS",
     github: "https://github.com/foresthpark/PaintJS",
     tech: ["HTML", "CSS", "JS"],
   },
   {
-    name: "NYT Flutter Times",
-    website: "https://nyt-flutter.firebaseapp.com/#/",
-    github: "https://github.com/foresthpark/flutter_web_times",
-    tech: ["Flutter"],
-  },
-
-  {
     name: "ClicBitz",
-    website:
-      "https://play.google.com/store/apps/details?id=com.buildr.clicbitz&hl=en_US",
+    website: "https://clicbitz.com/",
     github: null,
     tech: ["Flutter", "Firebase"],
   },
